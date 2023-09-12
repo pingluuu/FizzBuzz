84a4338c4916e3a458019dfa868d348e03b4b4ba
Ping Lu <pinglu@Pings-MacBook-Pro.local>
2023-09-11 21:27:40 -0400

    finish task 5

--- src/Reduce.java
+++ src/Reduce.java
@@ -1,2 +1,26 @@
-package PACKAGE_NAME;public class Reduce {
+public class Reduce {
+    public static void main(String[] args) {
+        int i = 100;
+        int c = 0;
+        do_reduce(i, c);
+    }
+    public static void do_reduce(int i, int c) {
+        while (i > 0) {
+
+            // Find out which numbers divide i.
+            boolean even = i % 2 == 0;
+            boolean odd = i % 2 != 0;
+
+            // Print our appropriate result.
+            if (even) {
+                i = i/2;
+                c++;
+            } else {
+                i--;
+                c++;
+            }
+        }
+        System.out.println(c);
+    }
+
 }
