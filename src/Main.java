342cef347bae24b26af93eb6245ab4bd0813cb71
Ping Lu <pinglu@Pings-MacBook-Pro.local>
2023-09-11 20:58:44 -0400

    run FizzBuzz.Main from Main.Java

--- src/Main.java
+++ src/Main.java
@@ -1,2 +1,7 @@
-package PACKAGE_NAME;public class Main {
+public class Main {
+    public static void main(String[] args) {
+        FizzBuzz.do_FizzBuzz(5);
+        FizzBuzz.main(null);
+    }
+
 }
